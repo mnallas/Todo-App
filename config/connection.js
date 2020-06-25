696ba67b5a7c66f96bd99b718a475c497d0aa70a
myhkas <mmyhkas@gmail.com>
2020-06-24 19:52:57 -0700

    deployed to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Jock204dash189",
-  database: "todo_db",
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.connection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Jock204dash189",
+    database: "todo_db",
+  });
+}
 
 module.exports = connection;
